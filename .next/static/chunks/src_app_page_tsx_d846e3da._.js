2f6172fffb06502719e72c7f2b7728a7390e08d6
sallu1432 <sallu1432006@gmail.com>
2025-11-11 15:31:39 +0530

    Remove .next and .env from repository

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b6de158a._.js"
-],
-    source: "dynamic"
-});
